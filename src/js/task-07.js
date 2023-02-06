95b7a01751e6e5565c88df164518bcd9c5cce5da
Roman Petrenko <rpetrenko0105@gmail.com>
2023-02-06 15:12:32 +0200

    add task 7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -3,3 +3,16 @@
  * (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size.
  * В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
  */
+
+const inputControlEl = document.querySelector('#font-size-control');
+console.log(inputControlEl);
+const spanTextEl = document.querySelector('#text');
+console.log(spanTextEl);
+
+inputControlEl.addEventListener('input', callback);
+function callback(event) {
+  let fontSize = '';
+  fontSize = event.currentTarget.value;
+
+  spanTextEl.setAttribute('style', `font-size: ${fontSize}px`);
+}
